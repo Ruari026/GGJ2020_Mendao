8ebe9e31da264ce469c9d9efa49d53676fa534b0
Roelof Neuman <RNEUMA200@caledonian.ac.uk>
2020-02-01 18:46:52 +0000

    my version with bullshit
    test

---
+++ GGJ2020/Assets/Objects/Teleport.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    bool inRange;
+    public GameObject fishLeft;
+    public  GameObject fishRight;
+    bool left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishLeft = GameObject.Find("FishPrefab (Left)");
+        fishRight = GameObject.Find("FishPrefab (Right)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange == true && Input.GetKeyDown(KeyCode.T) )
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inRange = true; 
+        if (other.tag = "FishLeft")
+        {
+            left = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inRange = false;
+    }
+
+
+}
