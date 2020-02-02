14fb4fd134b05dbd68b7ea4a1de610e6131c69d6
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-02-02 11:36:29 +0000

    Menu Bugs DeBugged

---
+++ GGJ2020/Assets/Scripts/UI/EscapeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class EscapeController : MonoBehaviour
+{
+    public UnityEvent onSceneEscape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            onSceneEscape.Invoke();
+            StartCoroutine(WaitThenEscape());
+        }
+    }
+
+    private IEnumerator WaitThenEscape()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("MenuScene");
+    }
+}
