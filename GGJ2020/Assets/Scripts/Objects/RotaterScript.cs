7edee7d78a17cd043565ff284bc4834628d87d53
Roelof Neuman <RNEUMA200@caledonian.ac.uk>
2020-02-02 01:33:13 +0000

    attempt
    let's gooo, the rest

---
+++ GGJ2020/Assets/Scripts/Objects/RotaterScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.forward * 1.2f  * Time.deltaTime);
+    }
+}
